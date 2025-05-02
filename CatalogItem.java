7ff153f3888f92c02ec583727cf8354d6b7e3116
Omanhene20 <144060132+Omanhene20@users.noreply.github.com>
2025-05-02 07:23:07 -0500

    Create CatalogItem.java

---
+++ CatalogItem.java
@@ -0,0 +1,32 @@
+public class CatalogItem {
+    private Book book;
+    private String id;
+    private boolean availability;
+
+    // Constructor
+    public CatalogItem(Book book, String id, boolean availability) {
+        this.book = book;
+        this.id = id;
+        this.availability = availability;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isAvailable() {
+        return availability;
+    }
+
+    public void setAvailable() {
+        availability = true;
+    }
+
+    public void setUnavailable() {
+        availability = false;
+    }
+}
